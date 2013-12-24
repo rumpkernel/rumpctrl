841826cccf66a4d76c3fa50ea9871d9e1f0f7105
Justin Cormack <justin@specialbusservice.com>
2013-12-24 00:49:48 +0000

    closer to working, some stubs left

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+Unfinished, not working!
+
 This is a small experimental wrapper for running programs that were written for a normal POSIX (NetBSD) system to run them under rump kernel.
 
---
+++ stub.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+
+#define STUB(name)                              \
+  int name(void); int name(void) {              \
+        static int done = 0;                    \
+        errno = ENOTSUP;                        \
+        if (done) return ENOTSUP; done = 1;     \
+      /*printk("STUB ``%s'' called\n", #name);*/\
+        return ENOTSUP;}
+
+STUB(__clock_gettime50);
+STUB(__nanosleep50);
+STUB(__setitimer50);
+STUB(__sigaction14);
+STUB(__sigprocmask14);
+STUB(__getrusage50);
+
+STUB(_lwp_kill);
+STUB(_lwp_self);
+STUB(__wait450);
+STUB(__fork);
+STUB(__vfork14);
+STUB(execve);
+
