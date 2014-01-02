7d370e44c9414b198e63df54c8023ae554ac0069
Justin Cormack <justin@specialbusservice.com>
2014-01-02 17:50:28 +0000

    add exit.c

---
+++ exit.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <setjmp.h>
+
+static jmp_buf buf;
+
+int main(int argc, char **argv);
+
+int
+emul_main_wrapper(int argc, char **argv)
+{
+	int ret;
+
+	if (! (ret = setjmp(buf))) {
+        	return main(argc, argv);
+	}
+	return ret;
+}
+
+void
+_exit(int status)
+{
+	longjmp(buf, status);
+}
+
+
