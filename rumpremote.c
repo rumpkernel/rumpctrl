37d7e606e5cc77724acc177113ffef24932d652f
Antti Kantee <pooka@iki.fi>
2014-01-13 23:11:13 +0000

    using the greatest hack ever, make sure fd 0-2 are open
    (note that we can't use the normal "open /dev/null" since the
    rump kernel server might not support vfs)

--- rumpremote.c
+++ rumpremote.c
@@ -8,15 +8,20 @@
 #include "rumprun_common.c"
 
 #include <rump/rumpclient.h>
+#include <rump/rump_syscalls.h>
 
 int
 main(int argc, char *argv[])
 {
-	int ret;
+	int ret, fd;
 
 	ret = rumpclient_init();
 	if (ret != 0)
 		die("rumpclient init failed");
+	/* this has to be the greatest hack ever */
+	while ((fd = rump_sys_kqueue()) < 3)
+		continue;
+	rump_sys_close(fd);
 	rumprun_so(argc, argv);
 	/* NOTREACHED */
 	return 0;
