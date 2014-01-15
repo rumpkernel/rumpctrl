269e56f1bed75e0701d44448a7580667f8853a78
Justin Cormack <justin@specialbusservice.com>
2014-01-15 21:54:55 +0000

    add progname support

--- exit.c
+++ exit.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <setjmp.h>
 
+extern char *__progname;
+
 static jmp_buf buf;
 
 int main(int argc, char **argv);
@@ -10,6 +12,8 @@ int
 emul_main_wrapper(int argc, char **argv)
 {
 	int ret;
+
+	__progname = argv[0];
 
 	if (! (ret = setjmp(buf))) {
         	return main(argc, argv);
