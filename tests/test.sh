d0c9298ac89e258a3c12b73878ac1f05fcad0845
Justin Cormack <justin@specialbusservice.com>
2014-03-01 19:24:35 +0000

    somewhat of a test framework

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,53 @@
 
 # Initial test script to sanity check
 
+# set up environment
 export LD_LIBRARY_PATH=.:rumpdyn/lib
+EC=0
 
-./rumprun ifconfig
-./rumprun sysctl kern.hostname
-./rumprun df
+Basic_ifconfig()
+{
+echo "Basic ifconfig"
+./rumprun ifconfig | grep lo0 > /dev/null
+if [ $? -ne 0 ]
+then 
+	echo "ERROR Basic ifconfig"
+	EC=`expr $EC + 1`
+fi 
+}
+Basic_ifconfig
 
+Basic_sysctl()
+{
+echo "Basic sysctl"
+./rumprun sysctl kern.hostname | grep 'kern.hostname = rump-' > /dev/null
+if [ $? -ne 0 ]
+then 
+	echo "ERROR Basic sysctl"
+	EC=`expr $EC + 1`
+fi 
+}
+Basic_sysctl
+
+Basic_df()
+{
+echo "Basic df"
+./rumprun df | grep rumpfs > /dev/null
+if [ $? -ne 0 ]
+then
+	echo "ERROR Basic df"
+	EC=`expr $EC + 1`
+fi
+}
+Basic_df
+
+# output
+
+if [ $EC -ne 0 ]
+then
+	echo "FAIL: $EC tests failed"
+	exit 1
+else
+	echo "PASSED"
+	exit 0
+fi
